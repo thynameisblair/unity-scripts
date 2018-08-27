149c8e5e061aa95bc4c4575863d6ef90134edff8
Blair <thynameisblair@chaosandcode.com>
2018-08-27 00:01:53 -0700

    Add Loader.cs
    Uses SceneManager to additively load one or more Scenes.

---
+++ Loader.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+    private readonly Queue<LoadRequest> loadRequestQueue = null;
+    private LoadRequest activeLoadRequest = null;
+    private Coroutine activeCoroutine = null;
+    private Scene? lastLoadedScene = null;
+
+    public class LoadRequest
+    {
+        public class LoadScene
+        {
+            public string SceneName { get; set; }
+            public Scene LoadedScene { get; set; }
+        }
+        public List<LoadScene> Scenes
+        {
+            get; private set;
+        }
+        public object CompletionEventArg
+        {
+            get; private set;
+        }
+        public delegate void RequestCompletedDelegate(LoadRequest loadRequest);
+        public RequestCompletedDelegate OnRequestCompleted
+        {
+            get; set;
+        }
+
+        public LoadRequest(object completionEventArg = null)
+        {
+            Scenes = new List<LoadScene>();
+            CompletionEventArg = completionEventArg;
+        }
+        public void AddScene(string sceneName)
+        {
+            LoadScene loadScene = new LoadScene
+            {
+                SceneName = sceneName
+            };
+            Scenes.Add(loadScene);
+        }
+    }
+
+    public Loader()
+    {
+        loadRequestQueue = new Queue<LoadRequest>();
+    }
+
+    public void AddLoadRequest(LoadRequest loadRequest)
+    {
+        loadRequestQueue.Enqueue(loadRequest);
+        if (activeCoroutine == null)
+        {
+            activeCoroutine = StartCoroutine(ExecuteLoadRequests());
+        }
+    }
+
+    IEnumerator ExecuteLoadRequests()
+    {
+        while (loadRequestQueue.Count > 0)
+        {
+            activeLoadRequest = loadRequestQueue.Dequeue();
+
+            foreach (LoadRequest.LoadScene loadScene in activeLoadRequest.Scenes)
+            {
+                AsyncOperation op = SceneManager.LoadSceneAsync(loadScene.SceneName, LoadSceneMode.Additive);
+                op.allowSceneActivation = false;
+                while (op.progress < 0.9f)
+                {
+                    yield return null;
+                }
+                op.allowSceneActivation = true;
+                yield return null; // finish loading scene, OnSceneLoaded will be called
+                loadScene.LoadedScene = lastLoadedScene.Value;
+                lastLoadedScene = null;
+            }
+            if (activeLoadRequest.CompletionEventArg != null)
+            {
+                foreach (LoadRequest.LoadScene loadScene in activeLoadRequest.Scenes)
+                {
+                    foreach (GameObject go in loadScene.LoadedScene.GetRootGameObjects())
+                    {
+                        go.SendMessage("OnLoadRequestCompleted", activeLoadRequest.CompletionEventArg);
+                    }
+                }
+            }
+
+            if (activeLoadRequest.OnRequestCompleted != null)
+            {
+                activeLoadRequest.OnRequestCompleted(activeLoadRequest);
+            }
+            activeLoadRequest = null;
+        }
+        activeCoroutine = null;
+    }
+
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        lastLoadedScene = scene;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDestroy()
+    {
+        StopAllCoroutines();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# unity-scripts
+Assorted Unity scripts that I plug into Unity while I’m prototyping or doing other things.
+
+## Loader.cs
+Uses SceneManager to additively load one or more Scenes. You can use this to add functionality like “wait for all my scenes to be loaded before starting them” and “pass my scenes data so they have references to all my global scripts”.
